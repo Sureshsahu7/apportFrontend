c0723c416f816b571ef4f144722fb49515811cc5
Sureshsahu7 <85033183+Sureshsahu7@users.noreply.github.com>
2022-09-07 17:12:10 +0530

    Initial commit
    React counter

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(1);
+  const handleChange = (e) => {
+    if(e.target.value>=1 && e.target.value<=1000)
+      setCount(e.target.value)
+  }
+  const handleIncrement = () => {
+    if(parseInt(count)<1000)
+      setCount(parseInt(count)+1); 
+  }
+  const handleDecrement = () => {
+    if(count>=1)
+      setCount(count-1); 
+  }
+  return (
+    <div className="App">
+      <button className="decButton"  onClick={handleDecrement}>-</button>
+      <input className='counter'  type="number" value={count} onChange={handleChange} />
+      <button className="incButton" onClick={handleIncrement}>+</button>
+    </div>
+  );
+}
+
+export default App;
