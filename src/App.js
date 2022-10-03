20443a63904eaeeb5a3b35c44c05520afa70b3f7
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-10-03 18:17:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import { useState } from 'react';
 function App() {
   const [count, setCount] = useState(1);
   const handleChange = (e) => {
-    if(e.target.value>=1 && e.target.value<=1000)
+    if (e.target.value >= 1 && e.target.value <= 1000)
       setCount(e.target.value)
   }
   const handleIncrement = () => {
-    if(parseInt(count)<1000)
-      setCount(parseInt(count)+1); 
+    if (parseInt(count) < 1000)
+      setCount(parseInt(count) + 1);
   }
   const handleDecrement = () => {
-    if(count>=1)
-      setCount(count-1); 
+    if (count >= 1)
+      setCount(count - 1);
   }
   return (
     <div className="App">
-      <button className="decButton"  onClick={handleDecrement}>-</button>
-      <input className='counter'  type="number" value={count} onChange={handleChange} />
+      <h1>Apport Interview Purpose</h1>
+      <button className="decButton" onClick={handleDecrement}>-</button>
+      <input className='counter' type="number" value={count} onChange={handleChange} />
       <button className="incButton" onClick={handleIncrement}>+</button>
     </div>
   );
